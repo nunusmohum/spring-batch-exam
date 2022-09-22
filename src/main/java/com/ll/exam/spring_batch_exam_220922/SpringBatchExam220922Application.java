6468e05179916708040dc9e36dfe0e8d6102a3ae
nunusmohum <halojw301@gmail.com>
2022-09-22 09:25:18 +0900

    first setup

---
+++ src/main/java/com/ll/exam/spring_batch_exam_220922/SpringBatchExam220922Application.java
@@ -0,0 +1,13 @@
+package com.ll.exam.spring_batch_exam_220922;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchExam220922Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBatchExam220922Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/ll/exam/spring_batch_exam_220922/SpringBatchExam220922ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.exam.spring_batch_exam_220922;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchExam220922ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
